4a57803399c9fec6dc29f2d25a45dc0093bfc3a2
Aldona Tomzik <a.tomzik@gmail.com>
2021-04-06 15:25:28 +0200

    Extracted DailyCard component and add some css fixes

---
+++ src/components/DailyCard.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Icon from "./Icon";
+import { getWeekday } from "../utils";
+import "./DailyCard.css";
+
+function DailyCard({ dailyForecast }) {
+  return (
+    <div className="DailyCard" key={dailyForecast.dt}>
+      <p className="DailyCard__weekday">
+        {getWeekday(dailyForecast.dt, dailyForecast.timezone)}
+      </p>
+      <Icon iconId={dailyForecast.weather[0].icon} />
+      <p className="DailyCard_temp">
+        <span>{Math.round(dailyForecast.temp.max)}&deg;C</span>
+        <span>{Math.round(dailyForecast.temp.min)}&deg;C</span>
+      </p>
+    </div>
+  );
+}
+
+export default DailyCard;
--- src/components/DailyWeather.js
+++ src/components/DailyWeather.js
@@ -1,22 +1,12 @@
 import React from "react";
-import { getWeekday } from "../utils";
-import Icon from "./Icon";
+import DailyCard from "./DailyCard";
 import "./DailyWeather.css";
 
 function DailyWeather({ weatherData }) {
   return (
     <div className="DailyWeather">
       {weatherData.map((dailyForecast) => (
-        <div className="DailyCard" key={dailyForecast.dt}>
-          <p className="DailyCard__weekday">
-            {getWeekday(dailyForecast.dt, dailyForecast.timezone)}
-          </p>
-          <Icon iconId={dailyForecast.weather[0].icon} />
-          <p className="DailyCard_temp">
-            <span>{Math.round(dailyForecast.temp.max)}&deg;C</span>
-            <span>{Math.round(dailyForecast.temp.min)}&deg;C</span>
-          </p>
-        </div>
+        <DailyCard dailyForecast={dailyForecast} />
       ))}
     </div>
   );
